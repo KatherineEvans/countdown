65df2dc02bd633fc50c66f10d2cda3c69abc27ac
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-10-29 16:59:11 -0500

    Countdown functionality

--- EventList.js
+++ EventList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, Text, StyleSheet } from 'react-native';
 import EventCard from './EventCard';
 
 class EventList extends Component {
@@ -8,6 +8,15 @@ class EventList extends Component {
   }
 
   componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        events: this.state.events.map(evt => ({
+          ...evt,
+          timer: Date.now(),
+        })),
+      });
+    }, 1000);
+
     const events = require('./db.json').events.map(e => ({
       ...e,
       date: new Date(e.date),
@@ -18,6 +27,7 @@ class EventList extends Component {
   render() {
     return (
         <FlatList
+          style={styles.list}
           data={this.state.events}
           renderItem={({ item }) => <EventCard event={item} />}
           keyExtractor={item => item.id}
@@ -25,5 +35,13 @@ class EventList extends Component {
       );
   }
 }
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1, 
+    paddingTop: 75,
+    backgroundColor: '#F3F3F3'
+  },
+})
 
 export default EventList;
