200c771d22695a35d58af2c89b28e2ea3b394146
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-10-29 11:00:46 -0500

    FlatList Component

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import EventList from './EventList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hi Katherine!</Text>
-        <Text>AAHHH</Text>
-      </View>
+      <EventList />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
